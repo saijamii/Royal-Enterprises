fdcc1718b44372b79071319dda8efa3f9733ec7f
saijami <saijami@unosimple.com>
2023-11-21 17:42:56 +0530

    addInventory added

--- Node/api.js
+++ Node/api.js
@@ -3,27 +3,17 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = `mongodb+srv://saijami:EcUpT3Et6dpojJz3@atlascluster.iotmmxp.mongodb.net/?retryWrites=true&w=majority`;
 
-const app = express();
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const dataBase = client.db("royal");
+const collection = dataBase.collection("products");
 
-const getInventory = async () => {
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-  const dataBase = client.db("royal");
-  try {
-    const collection = dataBase.collection("products");
-    const result = await collection.find().toArray();
-    return result.slice(0, 100);
-  } catch (error) {
-    console.log(`ERROR : ${error}`);
-  } finally {
-    client.close();
-  }
-};
+const app = express();
 
 app.get("/", (req, res) => {
   res.send(`API is Running on port ${PORT}`);
@@ -33,6 +23,34 @@ app.get("/inventoryProducts", async (req, res) => {
   const products = await getInventory();
   res.status(200).json(products);
 });
+
+const getInventory = async () => {
+  try {
+    const result = await collection.find().toArray();
+    return result.slice(0, 100);
+  } catch (error) {
+    console.log(`ERROR : ${error}`);
+  } finally {
+    client.close();
+  }
+};
+
+app.post("/addInventory", (req, res) => {
+  console.log(req.body);
+  let dataJson = JSON.parse(JSON.stringify(req.body));
+  console.log(dataJson, "dataJson");
+  addProducts(dataJson);
+  res.status(200).json(dataJson);
+});
+
+const addProducts = async (dataJson) => {
+  try {
+    const result = await collection.insertOne(dataJson);
+    console.log(`Saved response with ID: ${result.insertedId}`);
+  } catch (error) {
+    console.error(`Error : ${error}`);
+  }
+};
 
 const PORT = 5000;
 app.listen(PORT, () => {
