565315b411c94d441b2e2b006726e3c7137c91f4
saijami <saijami@unosimple.com>
2023-11-22 15:47:18 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,109 @@
-import "./App.css";
+import React from "react";
+// import { Layout, Button, Col, Row } from "antd";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Application from "./Application";
+import "./App.css";
+import AddProduct from "./AddProduct";
+
+// const { Header } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <Application />
+    <div style={{ height: "100vh", overflowX: "hidden" }}>
+      <>
+        {/* <Header style={{ position: "fixed", width: "100%", zIndex: "100" }}>
+          <Col span={24}>
+            <div style={{ backgroundColor: "#8d99ae", height: "60px" }}>
+              <Col span={24}>
+                <Row style={{ justifyContent: "right" }}>
+                  <Col span={3} id="nav-links-container">
+                    <Button
+                      style={{
+                        backgroundColor: "#8d99ae",
+                        color: "#000",
+                        border: "0px",
+                        fontWeight: "600",
+                        fontSize: "20px",
+                        marginTop: "10px",
+                      }}
+                      onClick={() => (window.location.href = "/dashboard")}
+                    >
+                      DASHBOARD
+                    </Button>
+                  </Col>
+                  <Col span={2} id="nav-links-container">
+                    <Button
+                      style={{
+                        backgroundColor: "#8d99ae",
+                        color: "#000",
+                        border: "0px",
+                        fontSize: "20px",
+                        fontWeight: "600",
+                        marginTop: "10px",
+                      }}
+                      onClick={() => (window.location.href = "/homePage")}
+                    >
+                      HOME
+                    </Button>
+                  </Col>
+                  <Col span={2}>
+                    <Button
+                      style={{
+                        backgroundColor: "#8d99ae",
+                        color: "#000",
+                        border: "0px",
+                        fontSize: "20px",
+                        fontWeight: "600",
+                        marginTop: "10px",
+                      }}
+                      onClick={() => (window.location.href = "/usersTable")}
+                    >
+                      USERS
+                    </Button>
+                  </Col>
+                  <Col span={2}>
+                    <Button
+                      style={{
+                        backgroundColor: "#8d99ae",
+                        color: "#000",
+                        border: "0px",
+                        fontSize: "20px",
+                        fontWeight: "600",
+                        marginTop: "10px",
+                      }}
+                      onClick={() => (window.location.href = "/demoApi")}
+                    >
+                      Demo Data
+                    </Button>
+                  </Col>
+                  <Col span={2}>
+                    <Button
+                      style={{
+                        backgroundColor: "#8d99ae",
+                        color: "#000",
+                        border: "0px",
+                        fontSize: "20px",
+                        fontWeight: "600",
+                        marginTop: "10px",
+                      }}
+                      onClick={() => (window.location.href = "/demoExcel")}
+                    >
+                      Demo Employee Data
+                    </Button>
+                  </Col>
+                </Row>
+              </Col>
+            </div>
+          </Col>
+        </Header> */}
+
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Application />} />
+            <Route path="/addProduct" element={<AddProduct />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     </div>
   );
 }
