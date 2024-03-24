a30ced3ebf769356a34637c530331f65a1866548
saijamii <saijami38@gmail.com>
2024-03-24 14:37:07 +0530

    Terminal Issues removed from App

--- src/Application.js
+++ src/Application.js
@@ -1,24 +1,7 @@
-import React, { useEffect, useState, useContext } from "react";
-import axios from "axios";
-import {
-  Table,
-  Popover,
-  Row,
-  Col,
-  Button,
-  Popconfirm,
-  Input,
-  notification,
-} from "antd";
+import React, { useState, useContext } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Layout, Menu } from "antd";
-import {
-  EllipsisOutlined,
-  DeleteOutlined,
-  PlusOutlined,
-} from "@ant-design/icons";
-import { Link } from "react-router-dom";
-import Loading from "./Common/Loading";
+import { Layout } from "antd";
+
 import { AppContext } from "./AppContext";
 import SiderMenu from "./Common/SiderMenu";
 import CommonHeader from "./Common/CommonHeader";
@@ -39,50 +22,9 @@ export default function Application() {
   };
 
   const { Header, Content } = Layout;
-  const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState("");
-  const [loading, setLoading] = useState(false);
-  const token = localStorage.getItem("token");
 
   const context = useContext(AppContext);
   console.log(context, "context");
-
-  useEffect(() => {
-    token && getInventoryData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  useEffect(() => {
-    if (!token) {
-      console.error("No token ");
-      notification.warning({
-        placement: "top",
-        message: "Unauthorized user",
-      });
-      setTimeout(() => {
-        window.location.href = "/";
-      }, 2000);
-    }
-  }, [token]);
-
-  const getInventoryData = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(
-        "https://node-kl1g.onrender.com/inventoryProducts",
-        {
-          headers: {
-            authorization: token,
-          },
-        }
-      );
-      setUsers(data);
-      setLoading(false);
-      console.log(data, "data");
-    } catch (error) {
-      console.error("Error fetching inventory data:", error);
-      setLoading(false);
-    }
-  };
 
   return (
     <Layout hasSider>
