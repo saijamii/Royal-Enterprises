7142d27ce172a520990076e5b34d9232fd332871
saijamii <saijami38@gmail.com>
2024-03-24 11:52:58 +0530

    AppRoutes Init

---
+++ src/Config/AppRoutes.js
@@ -0,0 +1,59 @@
+import { AppstoreFilled } from "@ant-design/icons";
+import Login from "../Login";
+import Register from "../Register";
+import Application from "../Application";
+import AddProduct from "../AddProduct";
+import ProductDetail from "../ProductDetail";
+import Users from "../Users";
+
+const AppRoutes = [
+  { key: "1", exact: true, path: "/signin", component: Login },
+  { key: "2", exact: true, path: "/signup", component: Register },
+  {
+    key: "3",
+    exact: true,
+    path: "/home",
+    component: Application,
+    title: "Application",
+    icon: <AppstoreFilled />,
+    isShowItem: true,
+  },
+  {
+    key: "4",
+    exact: true,
+    path: "/addProduct",
+    component: AddProduct,
+    title: "AddProduct",
+    icon: <AppstoreFilled />,
+    isShowItem: true,
+  },
+  {
+    key: "5",
+    exact: true,
+    path: "/getProductDetail/:id",
+    component: ProductDetail,
+    title: "ProductDetail",
+    icon: <AppstoreFilled />,
+    isShowItem: true,
+  },
+  {
+    key: "6",
+    exact: true,
+    path: "/users",
+    component: Users,
+    title: "Users",
+    icon: <AppstoreFilled />,
+    isShowItem: true,
+  },
+  {
+    key: "7",
+    exact: true,
+    path: "/addUser",
+    component: Register,
+    title: "addUser",
+    icon: <AppstoreFilled />,
+    isShowItem: true,
+  },
+];
+
+export default AppRoutes;
