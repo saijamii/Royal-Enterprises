9729133f79b1e4002765ad220cc14b1217e3144e
saijamii <saijami38@gmail.com>
2024-03-24 12:45:30 +0530

    Login Changes

--- src/Common/Authenticated.js
+++ src/Common/Authenticated.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import WithAuth from "./WithAuth";
 import Application from "../Application";
-import Login from "../Login";
+import Landing from "./Landing";
 
 const Authenticated = () => {
   const location = useLocation();
@@ -14,14 +14,13 @@ const Authenticated = () => {
         // marginBottom: "10vw",
       }}
     >
-      {location.pathname !== "/login" &&
-      location.pathname !== "*" &&
+      {location.pathname !== "*" &&
       location.pathname !== "/" &&
       location.pathname !== "" ? (
         <Application />
       ) : (
         <Routes>
-          <Route path="*" element={<Login />} />
+          <Route path="*" element={<Landing />} />
         </Routes>
       )}
     </div>
