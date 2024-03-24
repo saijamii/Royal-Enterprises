00efb6a7a9c42ca7e77fb152d4fdea798512461c
saijamii <saijami38@gmail.com>
2024-03-24 14:35:14 +0530

    App Routes Changes

--- src/Config/AppRoutes.js
+++ src/Config/AppRoutes.js
@@ -4,19 +4,20 @@ import Register from "../Register";
 import AddProduct from "../AddProduct";
 import ProductDetail from "../ProductDetail";
 import Users from "../Users";
+import Moives from "../JSON/Components/Moives";
 
 const AppRoutes = [
   { key: "1", exact: true, path: "/signin", component: Login },
   { key: "2", exact: true, path: "/signup", component: Register },
-  // {
-  //   key: "3",
-  //   exact: true,
-  //   path: "/home",
-  //   component: Application,
-  //   title: "Application",
-  //   icon: <AppstoreFilled />,
-  //   isShowItem: true,
-  // },
+  {
+    key: "3",
+    exact: true,
+    path: "/home",
+    component: Moives,
+    title: "Moives",
+    icon: <AppstoreFilled />,
+    isShowItem: true,
+  },
   {
     key: "4",
     exact: true,
