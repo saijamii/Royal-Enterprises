779a3666351f32ed4b129a14354e5261ccce9bf1
saijamii <saijami38@gmail.com>
2024-05-04 18:35:19 +0530

    formSignUp removed

--- src/Common/Landing.js
+++ src/Common/Landing.js
@@ -20,10 +20,10 @@ function Landing() {
   const context = useContext(AppContext);
   console.log(context, "context");
   const [formSignIn] = Form.useForm();
-  const [formSignUp] = Form.useForm();
+  // const [formSignUp] = Form.useForm();
   const { Content } = Layout;
   const [signin, setSignIn] = useState(false);
-  const [signup, setSignUp] = useState(false);
+  // const [signup, setSignUp] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -72,48 +72,48 @@ function Landing() {
     }
   };
 
-  const onFinishSignUp = async (values) => {
-    try {
-      setLoading(true);
-      const response = await axios.post(
-        "https://node-kl1g.onrender.com/sigup",
-        values
-      );
+  // const onFinishSignUp = async (values) => {
+  //   try {
+  //     setLoading(true);
+  //     const response = await axios.post(
+  //       "https://node-kl1g.onrender.com/sigup",
+  //       values
+  //     );
 
-      if (response.data.message === "success") {
-        notification.success({
-          placement: "top",
-          message: "User created successful!",
-        });
-        setLoading(false);
-        setTimeout(() => {
-          setSignUp(false);
-          setSignIn(true);
-        }, 1200);
-      } else if (
-        response.data.message === "Username already exists. Choose another one."
-      ) {
-        notification.error({
-          placement: "top",
-          message: "Username already exists. Choose another one",
-        });
-        setLoading(false);
-      } else {
-        setLoading(false);
-        notification["error"]({
-          placement: "top",
-          message: "Something went wrong!",
-        });
-      }
-    } catch (error) {
-      console.error("Sign in Error:", error);
-      setLoading(false);
-      notification.error({
-        placement: "top",
-        message: "Something went wrong!",
-      });
-    }
-  };
+  //     if (response.data.message === "success") {
+  //       notification.success({
+  //         placement: "top",
+  //         message: "User created successful!",
+  //       });
+  //       setLoading(false);
+  //       setTimeout(() => {
+  //         // setSignUp(false);
+  //         setSignIn(true);
+  //       }, 1200);
+  //     } else if (
+  //       response.data.message === "Username already exists. Choose another one."
+  //     ) {
+  //       notification.error({
+  //         placement: "top",
+  //         message: "Username already exists. Choose another one",
+  //       });
+  //       setLoading(false);
+  //     } else {
+  //       setLoading(false);
+  //       notification["error"]({
+  //         placement: "top",
+  //         message: "Something went wrong!",
+  //       });
+  //     }
+  //   } catch (error) {
+  //     console.error("Sign in Error:", error);
+  //     setLoading(false);
+  //     notification.error({
+  //       placement: "top",
+  //       message: "Something went wrong!",
+  //     });
+  //   }
+  // };
 
   return (
     <Layout>
@@ -142,9 +142,9 @@ function Landing() {
             className="login-header-signin"
             onClick={() => {
               formSignIn.resetFields();
-              formSignUp.resetFields();
+              // formSignUp.resetFields();
               setSignIn(false);
-              setSignUp(false);
+              // setSignUp(false);
             }}
             style={{ minWidth: "50px" }}
             icon={<HomeOutlined />}
@@ -157,26 +157,26 @@ function Landing() {
               <Button
                 className="login-header-signin"
                 onClick={() => {
-                  formSignUp.resetFields();
+                  // formSignUp.resetFields();
                   setSignIn(true);
-                  setSignUp(false);
+                  // setSignUp(false);
                 }}
               >
                 Sign in
               </Button>
             </Col>
-            <Col>
+            {/* <Col>
               <Button
                 className="login-header-signup"
                 onClick={() => {
                   formSignIn.resetFields();
                   setSignIn(false);
-                  setSignUp(true);
+                  // setSignUp(true);
                 }}
               >
                 Sign up
               </Button>
-            </Col>
+            </Col> */}
           </Row>
         </Col>
       </Header>
@@ -189,7 +189,7 @@ function Landing() {
         }}
       >
         <Col>
-          {signin && !signup ? (
+          {signin ? (
             <Row justify="center" align="middle" className="login-container">
               <Col xxl={12} xl={12} lg={16} md={14} sm={14} xs={24}>
                 <Card
@@ -264,169 +264,11 @@ function Landing() {
                       onClick={() => {
                         formSignIn.resetFields();
                         setSignIn(false);
-                        setSignUp(true);
+                        // setSignUp(true);
                       }}
                     >
                       Signup
                     </span>
-                  </Form>
-                </Card>
-              </Col>
-            </Row>
-          ) : !signin && signup ? (
-            <Row justify="center" align="middle" className="login-container">
-              <Col xxl={12} xl={12} lg={16} md={14} sm={14} xs={24}>
-                <Card
-                  className="login-card"
-                  style={{
-                    boxShadow: "0 4px 8px 0 rgba(0, 0, 8, 0.2)",
-                    backgroundColor: "#c7c8cc",
-                    border: "transparent",
-                  }}
-                >
-                  {/* <h1 className="login-title">Create account</h1> */}
-                  <Form
-                    form={formSignUp}
-                    onFinish={onFinishSignUp}
-                    autoComplete="off"
-                    className="login-form"
-                  >
-                    <Col>
-                      <span>
-                        First Name<span style={{ color: "red" }}>*</span>
-                      </span>
-                      <Form.Item
-                        name="firstName"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Please input your First Name!",
-                          },
-                        ]}
-                      >
-                        <Input
-                          placeholder="First Name"
-                          className="form-input"
-                        />
-                      </Form.Item>
-                    </Col>
-
-                    <Col>
-                      <span>
-                        Last Name<span style={{ color: "red" }}>*</span>
-                      </span>
-                      <Form.Item
-                        name="lastName"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Please input your Last Name!",
-                          },
-                        ]}
-                      >
-                        <Input
-                          placeholder="Input Last Name"
-                          className="form-input"
-                        />
-                      </Form.Item>
-                    </Col>
-
-                    <Col>
-                      <span>
-                        Email<span style={{ color: "red" }}>*</span>
-                      </span>
-                      <Form.Item
-                        name="userId"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Please Enter Your Email!",
-                          },
-                          {
-                            type: "email",
-                            message: "The Input Is Not Valid Email",
-                          },
-                        ]}
-                      >
-                        <Input
-                          className="form-input"
-                          placeholder="Please Enter Your Email"
-                          prefix={
-                            <MailOutlined className="site-form-item-icon" />
-                          }
-                        />
-                      </Form.Item>
-                    </Col>
-
-                    <Col>
-                      <span className="title_changes ">
-                        Password<span style={{ color: "red" }}>*</span>
-                      </span>
-
-                      <Form.Item
-                        name="password"
-                        rules={[
-                          {
-                            required: true,
-                            pattern:
-                              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%#^*?&+=_-])[A-Za-z\d$@$!%*?&]/,
-                            message:
-                              "At least one  upperCase letter, At least one number and one special character!",
-                          },
-                        ]}
-                        hasFeedback
-                      >
-                        <Input.Password
-                          placeholder="Password"
-                          autoComplete="new-password"
-                          className="form-input"
-                          prefix={
-                            <LockOutlined className="site-form-item-icon" />
-                          }
-                        />
-                      </Form.Item>
-                    </Col>
-
-                    <Form.Item>
-                      <Button
-                        type="primary"
-                        htmlType="submit"
-                        className="login-form-button"
-                        loading={loading}
-                      >
-                        Submit Details
-                      </Button>
-                    </Form.Item>
-                    <Col>
-                      <Row justify={"space-between"}>
-                        <span
-                          style={{
-                            cursor: "pointer",
-                            color: "#4f46e5",
-                          }}
-                          onClick={() => {
-                            formSignIn.resetFields();
-                            formSignUp.resetFields();
-                            setSignIn(false);
-                            setSignUp(false);
-                          }}
-                        >
-                          Home
-                        </span>
-                        <span
-                          style={{
-                            cursor: "pointer",
-                            color: "#4f46e5",
-                          }}
-                          onClick={() => {
-                            setSignIn(true);
-                            setSignUp(false);
-                          }}
-                        >
-                          Back to Login
-                        </span>
-                      </Row>
-                    </Col>
                   </Form>
                 </Card>
               </Col>
